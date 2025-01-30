7494ccd0d5c20068b6dbebcfcb1726766515abac
BaSant <basssnthaider@gamil.com>
2025-01-31 01:25:26 +0200

    refactor:use the base response

--- src/main/java/com/GP/GP/roles/user/service/impl/AdmissionRequestImpl.java
+++ src/main/java/com/GP/GP/roles/user/service/impl/AdmissionRequestImpl.java
@@ -12,10 +12,13 @@ import com.GP.GP.roles.user.exception.InvalidOperationException;
 import com.GP.GP.roles.user.exception.ResourceNotFoundException;
 import com.GP.GP.roles.user.mapper.AdmissionRequestMapper;
 import com.GP.GP.utill.Enums;
+import com.GP.GP.utill.base.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -32,7 +35,7 @@ public class AdmissionRequestImpl implements AdmissionRequestService {
     private UniversityService universityService;
 
     @Override
-    public AdmissionRequestDTO createAdmissionRequest(AdmissionRequestDTO admissionRequestDTO) {
+    public ResponseEntity<Object> createAdmissionRequest(AdmissionRequestDTO admissionRequestDTO) {
         User user = userRepository.findById(admissionRequestDTO.getUserId())
                 .orElseThrow(() -> new ResourceNotFoundException("User not found with ${admissionRequestDTO.getUserId()}"));
         University university = universityService.findUniversityById(admissionRequestDTO.getUniversityId());
@@ -46,11 +49,15 @@ public class AdmissionRequestImpl implements AdmissionRequestService {
 
         AdmissionRequest savedRequest = admissionRequestRepository.save(admissionRequest);
 
-        return AdmissionRequestMapper.toDTO(savedRequest);
+        AdmissionRequestDTO dto = AdmissionRequestMapper.toDTO(savedRequest);
+        BaseResponse response = new BaseResponse(true, "Admission request created successfully", dto);
+
+
+        return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
     @Override
-    public AdmissionRequestDTO updateAdmissionRequest(int id, AdmissionRequestDTO admissionRequestDTO) {
+    public ResponseEntity<Object> updateAdmissionRequest(int id, AdmissionRequestDTO admissionRequestDTO) {
         AdmissionRequest existingRequest = admissionRequestRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Admission request not found"));
 
@@ -61,14 +68,18 @@ public class AdmissionRequestImpl implements AdmissionRequestService {
         existingRequest.setHousingType(admissionRequestDTO.getHousingType());
         existingRequest.setUpdatedAt(LocalDateTime.now());
         AdmissionRequest updatedRequest = admissionRequestRepository.save(existingRequest);
-        return AdmissionRequestMapper.toDTO(updatedRequest);
+        AdmissionRequestDTO dto = AdmissionRequestMapper.toDTO(updatedRequest);
+        BaseResponse response =new BaseResponse(true, "Admission request updated successfully", dto);
+        return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
     @Override
-    public String checkApplicationStatus(int id, int userId) {
+    public ResponseEntity<Object> checkApplicationStatus(int id, int userId) {
         AdmissionRequest request = admissionRequestRepository.findByIdAndUserId(id, userId)
                 .orElseThrow(() -> new ResourceNotFoundException("Admission request not found"));
-        return request.getStatus().name();
+        String admissionRequestStatues = request.getStatus().name();
+        BaseResponse response = new BaseResponse(true, "Application status retrieved successfully", admissionRequestStatues);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
 
