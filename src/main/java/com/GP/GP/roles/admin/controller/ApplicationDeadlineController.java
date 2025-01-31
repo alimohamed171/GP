ecb763f5383153b27b1d79723fc99afb62a14973
YasmeenGad <gadyasmeen335@gmail.com>
2025-01-31 23:44:48 +0200

    feat: create app deadline dto

--- src/main/java/com/GP/GP/roles/admin/controller/ApplicationDeadlineController.java
+++ src/main/java/com/GP/GP/roles/admin/controller/ApplicationDeadlineController.java
@@ -1,7 +1,11 @@
 package com.GP.GP.roles.admin.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.GP.GP.repository.ApplicationDeadlineRepository;
+import com.GP.GP.roles.admin.models.dto.request.ApplicationGuidelineAndApprovalDTO;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("")
---
+++ src/main/java/com/GP/GP/roles/admin/models/dto/request/ApplicationDeadlineDTO.java
@@ -0,0 +1,29 @@
+package com.GP.GP.roles.admin.models.dto.request;
+
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ApplicationDeadlineDTO {
+
+    @NotNull(message = "Application start date is required")
+    @Future(message = "Start date must be in the future")
+    private LocalDate applicationStartDate;
+
+    @NotNull(message = "Application end date is required")
+    @Future(message = "End date must be in the future")
+    private LocalDate applicationEndDate;
+
+    @NotBlank(message = "Student type is required")
+    private String studentType;
+}
