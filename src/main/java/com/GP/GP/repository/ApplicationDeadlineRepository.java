dcf774d157112b353649fa218d72ac858273395e
YasmeenGad <gadyasmeen335@gmail.com>
2025-01-31 23:25:01 +0200

    feat: create app deadline repo

---
+++ src/main/java/com/GP/GP/repository/ApplicationDeadlineRepository.java
@@ -0,0 +1,7 @@
+package com.GP.GP.repository;
+
+import com.GP.GP.entities.ApplicationDeadline;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ApplicationDeadlineRepository extends JpaRepository<ApplicationDeadline, Integer> {
+}
