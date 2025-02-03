cdaaf0633f0479763ddf6d683db1571ef2a3dae1
BaSant <basssnthaider@gamil.com>
2025-02-03 19:21:58 +0200

    feat:add accommodation Dto and mapper

---
+++ src/main/java/com/GP/GP/roles/user/mapper/AccommodationMapper.java
@@ -0,0 +1,37 @@
+package com.GP.GP.roles.user.mapper;
+
+import com.GP.GP.entities.*;
+import com.GP.GP.roles.user.dto.AccommodationDTO;
+import lombok.Builder;
+
+@Builder
+public class AccommodationMapper {
+    public static AccommodationDTO toDTO(Accommodation accommodation) {
+        if (accommodation == null) {
+            return null;
+        }
+        return AccommodationDTO.builder()
+                .id(accommodation.getId())
+                .admissionRequestId(accommodation.getAdmissionRequest() != null ? accommodation.getAdmissionRequest().getId() : null)
+                .studentProfileId(accommodation.getStudentProfile() != null ? accommodation.getStudentProfile().getId() : null)
+                .housingTypeId(accommodation.getHousingType() != null ? accommodation.getHousingType().getId() : null)
+                .roomId(accommodation.getRoom() != null ? accommodation.getRoom().getId() : null)
+                .status(accommodation.getStatus())
+                .build();
+    }
+
+    public static Accommodation toEntity(AccommodationDTO dto, AdmissionRequest admissionRequest, StudentProfile studentProfile, HousingType housingType, Room room) {
+        if (dto == null) {
+            return null;
+        }
+
+        return Accommodation.builder()
+                .id(dto.getId())
+                .admissionRequest(admissionRequest)
+                .studentProfile(studentProfile)
+                .housingType(housingType)
+                .room(room)
+                .status(dto.getStatus())
+                .build();
+    }
+}
