6d3cd30c3b3abb0bf802ed6c8364189940bf69c5
BaSant <basssnthaider@gamil.com>
2025-02-21 16:08:11 +0200

    refactor: add a building type field, time stamps and add needed annotations.

--- src/main/java/com/GP/GP/entities/Building.java
+++ src/main/java/com/GP/GP/entities/Building.java
@@ -1,11 +1,19 @@
 package com.GP.GP.entities;
 
+import com.GP.GP.utill.Enums;
 import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "buildings")
 public class Building {
 
@@ -16,12 +24,22 @@ public class Building {
     @Column(name = "name")
     private String name;
 
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 10)
+    private Enums.BuildingType type; // MALE, FEMALE
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "university_id", nullable = false)
     private University university;
 
-    @OneToMany(mappedBy = "building", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "building", cascade = CascadeType.PERSIST,orphanRemoval = false)
     private List<Room> rooms = new ArrayList<>();
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
 
     // Getters and Setters
 }
