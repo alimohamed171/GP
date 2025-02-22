c1800eaa24477bb748a20e611e29e7cf9168e890
YasmeenGad <gadyasmeen335@gmail.com>
2025-02-22 05:58:18 +0200

    feat: add building repo

---
+++ src/main/java/com/GP/GP/repository/BuildingRepository.java
@@ -0,0 +1,6 @@
+package com.GP.GP.repository;
+
+import com.GP.GP.entities.Building;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BuildingRepository extends JpaRepository<Building, Integer> {}
