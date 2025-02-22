86048b472c80b8dc96eccdb8a90ae1aa76bc6175
YasmeenGad <gadyasmeen335@gmail.com>
2025-02-22 18:32:49 +0200

    fix: add service annotation to solve issue

--- src/main/java/com/GP/GP/roles/admin/service/impl/RoomServiceImpl.java
+++ src/main/java/com/GP/GP/roles/admin/service/impl/RoomServiceImpl.java
@@ -12,9 +12,11 @@ import com.GP.GP.utill.base.BaseResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Service
 public class RoomServiceImpl implements RoomService {
 
     @Autowired
