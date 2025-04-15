85f19e6ffee40c0c09053767f3bef3c15f8fa1f6
YasmeenGad <gadyasmeen335@gmail.com>
2025-04-16 00:39:16 +0300

    feat: impl endpoint of meals

---
+++ src/main/java/com/GP/GP/roles/admin/controller/MealController.java
@@ -0,0 +1,37 @@
+package com.GP.GP.roles.admin.controller;
+
+import com.GP.GP.roles.admin.models.dto.request.MealRequestDTO;
+import com.GP.GP.roles.admin.service.contracts.MealService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("")
+public class MealController {
+    @Autowired
+    private MealService mealService;
+
+    @PostMapping("admin/add-meals")
+    public ResponseEntity<Object> createMeal(@Valid @RequestBody MealRequestDTO mealRequestDTO) {
+        return mealService.createMeal(mealRequestDTO);
+    }
+
+    @GetMapping("admin/get-meals")
+    public ResponseEntity<Object> getAllMeals() {
+        return mealService.getAllMeals();
+    }
+
+    @DeleteMapping("admin/delete-meal/{mealId}")
+    public ResponseEntity<Object> deleteMeal(@PathVariable int mealId) {
+        return mealService.deleteMealById(mealId);
+    }
+
+    @PutMapping("admin/delete-meal/{mealId}")
+    public ResponseEntity<Object> updateMeal(
+            @PathVariable int mealId,
+            @Valid @RequestBody MealRequestDTO mealRequestDTO) {
+        return mealService.updateMeal(mealId, mealRequestDTO);
+    }
+}
---
+++ src/main/java/com/GP/GP/roles/admin/models/mapper/MealMapper.java
@@ -0,0 +1,34 @@
+package com.GP.GP.roles.admin.models.mapper;
+
+import com.GP.GP.entities.Meal;
+import com.GP.GP.roles.admin.models.dto.request.MealRequestDTO;
+import com.GP.GP.roles.admin.models.dto.response.MealResponseDTO;
+
+public class MealMapper {
+
+    public static MealResponseDTO mapToResponseEntity(Meal meal) {
+        return MealResponseDTO.builder()
+                .id(meal.getId())
+                .mealDate(meal.getMealDate())
+                .mealType(meal.getMealType())
+                .description(meal.getDescription())
+                .image(meal.getImage())
+                .build();
+    }
+
+    public static Meal mapToRequestEntity(MealRequestDTO dto) {
+        return Meal.builder()
+                .mealDate(dto.getMealDate())
+                .mealType(dto.getMealType())
+                .description(dto.getDescription())
+                .image(dto.getImage())
+                .build();
+    }
+
+    public static void updateMealEntity(Meal meal, MealRequestDTO dto) {
+        meal.setMealDate(dto.getMealDate());
+        meal.setMealType(dto.getMealType());
+        meal.setDescription(dto.getDescription());
+        meal.setImage(dto.getImage());
+    }
+}
