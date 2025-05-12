23324db97fc2aa7f761124a40daf0f311c2e8024
BaSant <basssnthaider@gamil.com>
2025-05-13 01:50:22 +0300

    feat: create email service to custom email body and subject

---
+++ src/main/java/com/GP/GP/roles/Auth/service/EmailService.java
@@ -0,0 +1,23 @@
+package com.GP.GP.roles.Auth.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendResetEmail(String to, String token) {
+        String resetLink = "http://localhost:8080/public/reset-password?token=" + token;
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject("Reset Your Password");
+        message.setText("Click the link to reset your password: " + resetLink + "\n\nNote that this link is valid for only 10 minutes.");
+        mailSender.send(message);
+    }
+}
