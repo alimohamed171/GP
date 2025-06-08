78b1f15bf020abc1525f1ac095525e552aa90b23
ali <ali@visions-tech.net>
2025-06-09 01:38:19 +0300

    Feat: fix issue in Privileges

---
+++ src/main/java/com/GP/GP/roles/user/model/dto/PrivilegesDTO.java
@@ -0,0 +1,13 @@
+package com.GP.GP.roles.user.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PrivilegesDTO {
+    private Integer id;
+    private String name;
+}
